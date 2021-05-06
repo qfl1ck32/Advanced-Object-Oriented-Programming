b5f84076af01aa440353b26db09dba50b1605a3b
qfl1ck32 <qfl1ck32@yahoo.com>
2021-05-06 09:09:01 +0300

    Add show() method

--- src/Singletons/Menu.java
+++ src/Singletons/Menu.java
@@ -1,20 +1,27 @@
 package Singletons;
 
-import Records.MenuOption;
-
-import java.util.ArrayList;
-import java.util.HashMap;
-
 import DatabaseMock.WithArrayAndMap;
-import interfaces.IterableAndMappable;
 import JSONParser.JSONParser;
+import Records.MenuOption;
+import Services.UserSignUpService;
+import enumerations.LoginResponse;
+import enumerations.SignupResponse;
+import interfaces.IterableAndMappable;
+
+import java.util.Comparator;
+import java.util.HashMap;
 
 public class Menu extends WithArrayAndMap<MenuOption> implements IterableAndMappable {
     
     private final HashMap <String, Menu> submenus;
     public static Menu menu;
 
-    public static Menu getInstance() {
+    {
+        submenus = new HashMap <> ();
+    }
+
+
+    public static Menu getInstance() throws Exception {
         if (menu == null) {
             menu = new Menu("src/assets/menu.json");
         }
@@ -22,23 +29,15 @@ public class Menu extends WithArrayAndMap<MenuOption> implements IterableAndMapp
         return menu;
     }
 
-    {
-        submenus = new HashMap <> ();
-    }
-
     public void append(Object o) {
         MenuOption mo = (MenuOption) o;
 
-        super.arr.add(0, mo);
-        super.map.put(mo.name(), mo);
-    }
-
-    public ArrayList <MenuOption> getMenuOptions() {
-        return this.arr;
+        super.itemsArray.add(0, mo);
+        super.itemsMap.put(mo.name(), mo);
     }
 
     private void init(String filename, Object JSON) {
-        JSONParser parser = null;
+        JSONParser parser;
 
         if (filename != null)
             parser = new JSONParser(filename);
@@ -62,6 +61,8 @@ public class Menu extends WithArrayAndMap<MenuOption> implements IterableAndMapp
 
             this.append(menuOption);
         }
+
+        this.itemsArray.sort(Comparator.comparing(MenuOption::name));
     }
 
     private Menu(String filename) {
@@ -70,5 +71,97 @@ public class Menu extends WithArrayAndMap<MenuOption> implements IterableAndMapp
 
     private Menu(Object JSON) {
         init(null, JSON);
+    }
+
+    public boolean show() {
+        int index = 0;
+        boolean returnValue = true;
+
+        HashMap<Integer, String> mp = new HashMap<>();
+
+        for (MenuOption option : itemsArray) {
+            if (CurrentUser.getUser().isLoggedIn() && option.hideOnAuth())
+                continue;
+
+            if (!CurrentUser.getUser().isLoggedIn() && option.requiresAuth())
+                continue;
+
+            System.out.println(++index + ". " + option.name());
+            mp.put(index, option.name());
+        }
+
+        System.out.print("> ");
+        MenuOption choice;
+
+        while ((choice = itemsMap.get(mp.get(scanner.nextInt()))) == null
+                || CurrentUser.getUser().isLoggedIn() && choice.hideOnAuth()
+                || !CurrentUser.getUser().isLoggedIn() && choice.requiresAuth()) {
+            System.out.println("Invalid choice. Please try again.");
+            System.out.print("> ");
+        }
+
+        System.out.println();
+        scanner.nextLine();
+
+        if (submenus.get(choice.name()) != null)
+            return submenus.get(choice.name()).show();
+
+        switch (choice.name().toLowerCase()) {
+            case "login" -> handleLogin();
+            case "logout" -> {
+                try {
+                    Authenticate.getInstance().logout();
+                }
+                catch (Exception e) {
+                    System.out.println("Couldn't log you out.");
+                }
+            }
+            case "register" -> handleRegister();
+            case "exit" -> returnValue = false;
+        }
+
+        return returnValue;
+    }
+
+    private void handleLogin() {
+        String username, password;
+
+        System.out.print("Username: ");
+        username = scanner.nextLine();
+
+        System.out.print("Password: ");
+        password = scanner.nextLine();
+
+        LoginResponse loginResponse;
+
+        loginResponse = Authenticate.getInstance().login(username, password);
+
+        switch (loginResponse) {
+            case SUCCESS -> System.out.println("Successfully logged in.");
+            case NOT_EXISTS -> System.out.println("The given username does not exist.");
+            case WRONG_PASSWORD -> System.out.println("Wrong password.");
+        }
+    }
+
+    private void handleRegister() {
+        String username, email, password;
+
+        System.out.print("Username: ");
+        username = scanner.nextLine();
+
+        System.out.print("Email: ");
+        email = scanner.nextLine();
+
+        System.out.print("Password: ");
+        password = scanner.nextLine();
+
+        SignupResponse response = UserSignUpService.registerUser(username, email, password);
+
+        switch(response) {
+            case USERNAME_ALREADY_USED -> System.out.println("Username is already in use.");
+            case EMAIL_ALREADY_USED -> System.out.println("Email is already in use.");
+            case SUCCESS -> System.out.println("You have successfully registered!");
+            case EMPTY_FIELDS -> System.out.println("Unexpected  empty fields.");
+        }
     }
 }
