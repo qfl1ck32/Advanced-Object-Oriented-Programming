c11d581014f128551fd05e3f74a5e49128d97024
qfl1ck32 <qfl1ck32@yahoo.com>
2021-05-06 09:09:14 +0300

    Show the menu on run

--- src/foodDeliveryApp/FoodDeliveryApp.java
+++ src/foodDeliveryApp/FoodDeliveryApp.java
@@ -1,15 +1,28 @@
 package FoodDeliveryApp;
 
+import JSONParser.JSONParser;
 import Singletons.Menu;
+
+import java.util.Arrays;
+import java.util.UUID;
+import java.util.stream.Stream;
+import Audit.Audit;
 
 public class FoodDeliveryApp {
 
-    private static String welcomeMessage;
-
-    private Menu menu;
+    private final String welcomeMessage;
+    private Menu menu = null;
 
     FoodDeliveryApp() {
         welcomeMessage = "Welcome to Food Delivery!";
+
+        try {
+            menu = Menu.getInstance();
+        }
+
+        catch (Exception err) {
+            System.out.println("Couldn't load Menu data.");
+        }
     }
 
     public void greet() {
@@ -21,5 +34,10 @@ public class FoodDeliveryApp {
         FoodDeliveryApp myFoodDeliveryApp = new FoodDeliveryApp();
 
         myFoodDeliveryApp.greet();
+
+        while(myFoodDeliveryApp.menu.show()) {
+            System.out.print("\033[H\033[2J");
+            System.out.flush();
+        }
     }
 }
