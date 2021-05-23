1876dae91a03cbbfc493d4fe392d4ac17e5eabbb
qfl1ck32 <qfl1ck32@yahoo.com>
2021-05-23 20:31:37 +0300

    Add audit utility

--- src/Audit/Audit.java
+++ src/Audit/Audit.java
@@ -1,48 +1,12 @@
 package Audit;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.text.SimpleDateFormat;
-import java.util.Date;
+import Database.*;
+
+import java.util.Arrays;
 
 public class Audit {
-    private static Audit audit;
-    private FileWriter fileWriter;
-
-    public static Audit getInstance() {
-        if (audit == null) {
-            audit = new Audit();
-        }
-
-        return audit;
+    public static void insertLog(String ID, String log) {
+        Database.getInstance().sendQuery("INSERT INTO audit " +
+                "VALUES (UNHEX(REPLACE(?, '-', '')), ?, ?);", Arrays.asList(ID, DatabaseUtil.getTimestamp(), log));
     }
-
-    private Audit() {
-        try {
-            fileWriter = new FileWriter("src/assets/audit.csv", true);
-        }
-
-        catch (Exception ignored) {}
-    }
-
-    private void append(String actionName) {
-        try {
-            StringBuilder line = new StringBuilder();
-            line.append(actionName)
-                    .append(",")
-                    .append(new SimpleDateFormat("HH:mm:ss | yyyy.MM.dd").format(new Date()))
-                    .append("\n");
-
-            fileWriter.append(line);
-            fileWriter.close();
-        }
-
-        catch (Exception ignored) {}
-    }
-
-    public void userRegistered() {
-        append("user registered");
-    }
-
 }
